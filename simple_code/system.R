c2b7c25390692fc8d7c1dca3a9788901fb16a406
Jung Jessica <49683693+3mofstudy@users.noreply.github.com>
2020-06-29 13:04:05 +0800

    Update and rename 系統設定.R to system.R

--- simple_code/system.R
+++ simple_code/system.R
@@ -1,8 +1,8 @@
-#載入資料
+# 載入資料
  　source("路徑")             # 載入外部程式
  　sink("路徑")+sink()        # 紀錄程式執行結果
   
-#語系設定  
+# 語系設定  
 lct <- Sys.getlocale("LC_TIME")　#取得當前的語言
 Sys.setlocale("LC_TIME", "C")　#設定語言
 Sys.setlocale("LC_TIME", lct)　#重新設定為原語言
